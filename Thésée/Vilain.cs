705127562697b5fe09558e46578e1a008c5b7c4c
BigBinotte <55589663+BigBinotte@users.noreply.github.com>
2019-11-28 13:15:49 -0500

    yeet

--- Thésée/Vilain.cs
+++ Thésée/Vilain.cs
@@ -23,13 +23,27 @@ namespace Thésée
          return true;
       }
 
-      Random Dé { get; } = new Random();
+      Random De { get; } = new Random();
       public Vilain(Point pos) : base(SYMBOLE, pos)
       {
       }
 
-      public override Choix Agir(Carte carte) =>
-         (Choix) Dé.Next((int)Choix.Droite, (int)Choix.Bas);
+        public override async Task<(Protagoniste, Choix)> Agir(Carte carte, CancellationToken jeton)
+        {
+            await Task.Delay(100);
+            int faireMouvement;
+
+            if (!jeton.IsCancellationRequested)
+            {
+                faireMouvement = De.Next(1, 5);
+                if (faireMouvement == 1)
+                    return ((this, (Choix)De.Next((int)Choix.Droite, (int)Choix.Bas)));
+                else
+                    return ((this, Choix.Rien));
+            }
+            else
+                return ((this, Choix.Rien));
+        }
 
       protected override bool DéplacerImpl(Point nouvellePosition)
       {
