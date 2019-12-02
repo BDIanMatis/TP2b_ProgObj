9781a9782cce532d44ed879e80b396f3bfbb05c8
Matis F <57538729+vex027@users.noreply.github.com>
2019-12-02 08:57:36 -0500

    yeet

--- Thésée/Héros.cs
+++ Thésée/Héros.cs
@@ -7,50 +7,13 @@ using System.Threading.Tasks;
 
 namespace Thésée
 {
-
-   public class Héros : Protagoniste
-   {
-      public const char SYMBOLE = 'T';
-      public Héros(Point pos) : base(SYMBOLE, pos)
-      {
-      }
-      public override async Task<Choix> Agir(Carte carte)
-      {
-         AfficherMenu(carte);
-         return LireChoix();
-      }
-      static void AfficherMenu(Carte carte)
-      {
-         Console.WriteLine($"Carte de format {carte.Hauteur} x {carte.Largeur}");
-         Console.WriteLine($"Entrez {(char)Choix.Quitter} pour quitter");
-      }
-      static Choix LireChoix()
-      {
-         Console.Write("Votre choix? ");
-         switch (Console.ReadKey(true).Key)
-         {
-            case ConsoleKey.Q:
-               return Choix.Quitter;
-            case ConsoleKey.DownArrow:
-               return Choix.Bas;
-            case ConsoleKey.UpArrow:
-               return Choix.Haut;
-            case ConsoleKey.LeftArrow:
-               return Choix.Gauche;
-            case ConsoleKey.RightArrow:
-               return Choix.Droite;
-            default:
-               return Choix.Rien;
-         }
-      }
-   }
     public class Héros : Protagoniste
     {
         public const char SYMBOLE = 'T';
         public Héros(Point pos) : base(SYMBOLE, pos)
         {
         }
-        public override async Task<(Protagoniste, Choix)> Agir(Carte carte,CancellationToken jeton)
+        public override async Task<(Protagoniste, Choix)> Agir(Carte carte, CancellationToken jeton)
         {
             AfficherMenu(carte);
             await Task.Delay(1000);
@@ -66,6 +29,9 @@ namespace Thésée
         static async Task<Choix> LireChoix(CancellationToken jeton)
         {
             Console.Write("Votre choix? ");
+            await Task.Run(() =>(
+
+
             switch (Console.ReadKey(true).Key)
             {
                 case ConsoleKey.Q:
@@ -80,7 +46,8 @@ namespace Thésée
                     return Choix.Droite;
                 default:
                     return Choix.Rien;
-            }
+            });
+            
         }
         /// <summary>
         /// 
